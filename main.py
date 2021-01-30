15835132a578349144bb96ea13f97d73f19f0c12
mdberkey <meberkey@wisc.edu>
2021-01-30 16:48:24 -0600

    venv

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import io
-import os
+import os,io
 from google.cloud import vision
-from google.cloud.vision import types
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] - R'api.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "vision_cred.json"
 
-client - vision.ImageAnnotatorClient()
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = 'cat.jpg'
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
 
